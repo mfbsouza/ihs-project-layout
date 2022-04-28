c194eb275dff0d40ec5b768ab8bc6b38aba46d0f
mfbsouza <mfbsouza.it@gmail.com>
2022-04-28 01:39:32 -0300

    ioctl_cmds.py: trying to convert c defines to python

--- scripts/ioctl_cmds.py
+++ scripts/ioctl_cmds.py
@@ -1,17 +1,17 @@
 #define RD_SWITCHES   _IO('a', 'a')
-RD_SWITCHES = ord('a') << (4*2) | 'a'
+RD_SWITCHES = ord('a') << (4*2) | 61
 
 #define RD_PBUTTONS   _IO('a', 'b')
-RD_PBUTTONS = ord('a') << (4*2) | 'b'
+RD_PBUTTONS = ord('a') << (4*2) | 62
 
 #define WR_L_DISPLAY  _IO('a', 'c')
-WR_L_DISPLAY = ord('a') << (4*2) | 'c'
+WR_L_DISPLAY = ord('a') << (4*2) | 63
 
 #define WR_R_DISPLAY  _IO('a', 'd')
-WR_R_DISPLAY = ord('a') << (4*2) | 'd'
+WR_R_DISPLAY = ord('a') << (4*2) | 64
 
 #define WR_RED_LEDS   _IO('a', 'e')
-WR_RED_LEDS = ord('a') << (4*2) | 'e'
+WR_RED_LEDS = ord('a') << (4*2) | 65
 
 #define WR_GREEN_LEDS _IO('a', 'f')
-WR_GREEN_LEDS = ord('a') << (4*2) | 'f'
+WR_GREEN_LEDS = ord('a') << (4*2) | 66
